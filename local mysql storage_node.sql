f009aec0d7329d1920f26a52e71158115be7a1bf
Drew Letcher <drew@drewletcher.net>
2020-04-18 23:40:33 -0500

    testing update

--- local mysql storage_node.sql
+++ local mysql storage_node.sql
@@ -1,18 +1,32 @@
 SHOW TABLES;
+DESCRIBE foo_schema;
+DESCRIBE foo_transfer;
+DESCRIBE foo_transform;
+DESCRIBE foo_dbtransform;
+DESCRIBE weather_forecast;
+DESCRIBE foo_schema_etl;
 DESCRIBE foo_schema_etl2;
 
 SELECT * FROM foo_schema;
 SELECT * FROM foo_transfer;
+SELECT * FROM foo_transform;
 SELECT * FROM foo_dbtransform;
-SELECT * FROM rest_forecast;
+SELECT * FROM weather_forecast;
+SELECT * FROM foo_schema_etl;
+SELECT * FROM foo_schema_etl2;
 
 DELETE FROM foo_schema;
 DELETE FROM foo_transfer;
+DELETE FROM foo_transform;
 DELETE FROM foo_dbtransform;
-DELETE FROM rest_forecast;
+DELETE FROM weather_forecast;
+DELETE TABLE foo_schema_etl;
+DELETE TABLE foo_schema_etl2;
 
 DROP TABLE foo_schema;
 DROP TABLE foo_transfer;
+DROP TABLE foo_transform;
 DROP TABLE foo_dbtransform;
-DROP TABLE rest_forecast;
+DROP TABLE weather_forecast;
+DROP TABLE foo_schema_etl;
 DROP TABLE foo_schema_etl2;
