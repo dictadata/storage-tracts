ca68c5fba51d26bd5f6df88935b678cf1c0acb1a
Drew Letcher <drew@drewletcher.net>
2021-01-12 14:10:37 -0600

    refactor config file to tracts file

--- storage-etl.js
+++ storage-etl.js
@@ -14,24 +14,27 @@ const transfer = require('./lib/transfer');
 const download = require('./lib/download');
 const upload = require('./lib/upload');
 
+// set program argument defaults
 const appArgs = {
-  command: 'transfer',
-  configfile: './etl_config.json',
-  tract: 'transfer'
+  command: '',
+  tractsFile: './etl_tracts.json',
+  tract: ''  // default tract name is the command name
 }
 
+/**
+ * parseArgs
+ *   only command is required
+ *   example process.argv  ["node.exe", "storage-etl.js", "-c", <tractsFile>, <command>, <tract>]
+ */
 function parseArgs() {
-  // process.argv = ["node.exe", "storage-etl.js", command, "-c", configfile, tract]
-  // only command is required
-
   const myArgs = {};
 
   let i = 2;
   while (i < process.argv.length) {
     if (process.argv[i] === "-c") {
-      // configFile
+      // tractsFile
       if (i + 1 < process.argv.length) {
-        myArgs.configfile = process.argv[i + 1];
+        myArgs.tractsFile = process.argv[i + 1];
         ++i;
       }
     }
@@ -46,6 +49,7 @@ function parseArgs() {
     ++i;
   }
 
+  // tract name defaults to command name
   if (myArgs.command && !myArgs.tract)
     myArgs.tract = myArgs.command;
 
@@ -63,26 +67,35 @@ function parseArgs() {
     parseArgs();
 
     if (!appArgs.command) {
-      console.log("Transfer, transform and codify data storage sources.");
+      console.log("Transfer, transform and codify data.");
       console.log("");
-      console.log("etl command [-c configfile] [tract-name]");
+      console.log("storage-etl command [-c tractsFile] [tractName]");
       console.log("");
       console.log("Commands:");
-      console.log("  config - create example etl_config.json file in the current directory");
-      console.log("  codify - determine storage encoding for a single schema");
-      console.log("  list - listing of schema names in data source");
-      console.log("  scan - scan data source to determine storage encoding by codifying multiple schemas");
-      console.log("  transfer - transfer data between data sources, with optional transforms");
-      console.log("  download - download from remote files systems to the local file system");
-      console.log("  upload - upload local files to remote file systems");
+      console.log("  config - create example etl_tracts.json file in the current directory.");
+      console.log("  codify - determine storage encoding by codifying a single data source schema.");
+      console.log("  list - listing of schema names in a data source.");
+      console.log("  scan - list data source and determine storage encoding by codifying multiple schemas.");
+      console.log("  transfer - transfer data between data sources with optional transforms.");
+      console.log("  download - download schemas from remote files system to the local file system.");
+      console.log("  upload - upload schemas from local file system to remote file system.");
+      console.log("");
+      console.log("tractsFile");
+      console.log("  Configuration file that defines tracts, plug-ins and logging.");
+      console.log("  Default configuration file is ./etl_tracts.json");
+      console.log("");
+      console.log("tractName");
+      console.log("  The tract to follow in the configuration file.");
+      console.log("  Default tractName is the command name.");
+
       return;
     }
 
     let tracts = {};
     if (appArgs.command !== 'config')
-      tracts = config.loadTracts(appArgs.configfile);
+      tracts = await config.loadTracts(appArgs.tractsFile);
     else {
-      config.create(appArgs.configfile);
+      await config.createTracts(appArgs.tractsFile);
       return 0;
     }
 
@@ -94,7 +107,8 @@ function parseArgs() {
 
     switch (appArgs.command) {
       case 'config':
-        config.create();
+        // should never get here, see above 'config' code
+        await config.createTracts();  
         break;
       case 'codify':
         retcode = await codify(tract);
