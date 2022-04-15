a55cff29007b5bd21c98c0c86ddc93668736525c
drewlab <drew@dictadata.org>
2022-04-15 12:07:04 -0500

    add foreach action

--- storage/etl/foreach.js
+++ storage/etl/foreach.js
@@ -4,55 +4,68 @@
 "use strict";
 
 const Storage = require("@dictadata/storage-junctions");
+const { SMT } = require("@dictadata/storage-junctions/types");
 const logger = require('./logger');
-
-const fs = require('fs');
-const path = require('path');
-const stream = require('stream').promises;
+const { onTract } = require('./actions');
 
 /**
  * List schemas at a locus
  * and perform an action on each schema.
  */
 module.exports = async (tract) => {
-  logger.info("foreach: " + tract.origin.smt);
+  logger.verbose("foreach: " + tract.origin.smt);
   let retCode = 0;
 
   var jo;
   try {
-    let transforms = tract.transform || tract.transforms || {};
+    // find tract(s) for loop processing
+    // any tracts with key names that aren't reserved
+    let lpTracts = {};
+    for (const [ key, value ] of Object.entries(tract)) {
+      if (key[ 0 ] !== "_" &&
+        ![ "action", "origin", "terminal", "transform" ].includes(key)) {
+        lpTracts[ key ] = value;
+      }
+    }
+
+    // get list of schemas from source
     if (!tract.origin.options) tract.origin.options = {};
-    let encoding = tract.origin.options.encoding || {};
-
     jo = await Storage.activate(tract.origin.smt, tract.origin.options);
-
-    // get list of schemas to foreach
     let { data: list } = await jo.list();
+    jo.relax();
 
-    // loop through files and codify
+    // loop through list and process each schema
     for (let entry of list) {
       logger.verbose(entry.name);
-      let pipes = [];
-      pipes.push(jo.createReader({ schema: entry.name }));
 
-      for (let [ tfType, toptions ] of Object.entries(transforms))
-        pipes.push(jo.createTransform(tfType, toptions));
+      for (const [ key, lpTract ] of Object.entries(lpTracts)) {
+        // replace the schema in origin.smt
+        if (typeof lpTract.origin.smt === "string")
+          lpTract.origin.smt = new SMT(lpTract.origin.smt);
+        lpTract.origin.smt.schema = entry.name;
 
-      pipes.push(jo.createTransform('codify', { encoding: encoding }));
+        // replace the schema in terminal.smt
+        if (tract.origin.options.terminal_smt) {
+          if (typeof lpTract.terminal.smt === "string")
+            lpTract.terminal.smt = new SMT(lpTract.terminal.smt);
+          lpTract.terminal.smt.schema = entry.name;
+        }
 
-      await stream.pipeline(pipes);
+        // string replacement terminal.output
+        if (lpTract.terminal.output) {
+          lpTract.terminal.output = lpTract.terminal.output.replace("${schema}", entry.name);
+        }
+
+        await onTract(key, lpTract);
+      }
     }
 
-    if (tract.terminal.output) {
-      logger.debug(JSON.stringify(encoding, null, " "));
-      logger.info("encoding saved to " + tract.terminal.output);
-      fs.mkdirSync(path.dirname(tract.terminal.output), { recursive: true });
-      fs.writeFileSync(tract.terminal.output, JSON.stringify(encoding, null, " "));
+    if (tract.terminal && tract.terminal.output) {
+      //logger.debug(JSON.stringify(<results>, null, " "));
+      //logger.info("results saved to " + tract.terminal.output);
+      //fs.mkdirSync(path.dirname(tract.terminal.output), { recursive: true });
+      //fs.writeFileSync(tract.terminal.output, JSON.stringify(<results>, null, " "));
     }
-    else {
-      console.log(JSON.stringify(encoding, null, " "));
-    }
-
   }
   catch (err) {
     logger.error(err);
