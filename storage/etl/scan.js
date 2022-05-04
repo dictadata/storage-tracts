f39400332c8df5dda71b1a5e95ad1080c247ecb3
drewlab <drew@dictadata.org>
2022-05-04 07:36:11 -0500

    add file exlusion list

--- storage/etl/scan.js
+++ storage/etl/scan.js
@@ -20,8 +20,8 @@ module.exports = async (tract) => {
 
   var jo;
   try {
-    // validate tract(s) for loop processing
-    // tracts with key names that aren't reserved
+    // validate sub-tract(s) for loop processing
+    // looking for tracts with names (key) that aren't reserved
     let lpTracts = {};
     for (const [ key, value ] of Object.entries(tract)) {
       if (![ "action", "origin", "terminal", "transform", "description" ].includes(key) &&
@@ -32,24 +32,52 @@ module.exports = async (tract) => {
     }
 
     // get list of schemas from source
-    if (!tract.origin.options) tract.origin.options = {};
+    if (!tract.origin.options)
+      tract.origin.options = {};
     jo = await Storage.activate(tract.origin.smt, tract.origin.options);
     let { data: list } = await jo.list();
     jo.relax();
+
+    // exlusions
+    let exclude = [];
+    if (tract.origin.options.exclude) {
+      for (let filespec of tract.origin.options.exclude) {
+        let rx = '^' + filespec + '$';
+        rx = rx.replace('.', '\\.');
+        rx = rx.replace('*', '.*');
+        rx = new RegExp(rx);
+        exclude.push(rx);
+      }
+    }
+
 
     // loop through list and process each schema
     for (let entry of list) {
       logger.info(entry.name);
 
-      // string replacements
+      // check exclusion list
+      let skip = false;
+      for (let rx of exclude) {
+        if (rx.test(entry.name)) {
+          skip = true;
+          break;
+        }
+      }
+      if (skip) {
+        logger.warn("excluded: " + entry.name);
+        continue;
+      }
+
+      // set string replacement values
       let replacements = {
         "${rpath}": entry.rpath || entry.name,
         "${name}": entry.name,
         "${schema}": entry.name.substr(0, entry.name.lastIndexOf("."))
       };
 
+      // loop thru tracts to process
       for (const [ key, lpTract ] of Object.entries(lpTracts)) {
-        // replace strings
+        // string replacements
         let txtTract = JSON.stringify(lpTract);
         for (const [ find, replace ] of Object.entries(replacements)) {
           txtTract = txtTract.replace(find, replace);
@@ -73,7 +101,7 @@ module.exports = async (tract) => {
     logger.info("=== completed");
   }
   catch (err) {
-    logger.error(err);
+    logger.error("scan: " + err.message);
     retCode = 1;
   }
   finally {
