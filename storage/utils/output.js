65650b9b3dcb4146da6f580af8874eb3d0b29a5d
drewlab <drew@dictadata.org>
2024-03-22 21:18:42 -0500

    add output to a stream

--- storage/utils/output.js
+++ storage/utils/output.js
@@ -9,16 +9,21 @@ const path = require('node:path');
 module.exports = exports = (output, data, compareValues = 2) => {
   let retCode = 0;
 
-  if (output && data) {
-    logger.info("output saved to " + output);
-    fs.mkdirSync(path.dirname(output), { recursive: true });
-    fs.writeFileSync(output, JSON.stringify(data, null, " "), "utf8");
-  }
+  if (typeof output === "string") {
+    if (data) {
+      logger.info("output saved to " + output);
+      fs.mkdirSync(path.dirname(output), { recursive: true });
+      fs.writeFileSync(output, JSON.stringify(data, null, " "), "utf8");
+    }
 
-  if (process.env.NODE_ENV === "development") {
-    // compare output
-    let expected = output.replace("output", "expected");
-    retCode = _compare(expected, output, compareValues);
+    if (process.env.NODE_ENV === "development") {
+      // compare output
+      let expected = output.replace("output", "expected");
+      retCode = _compare(output, expected, compareValues);
+    }
+  }
+  else if (output && output.writable) {
+    output.write(JSON.stringify(data, null, 2));
   }
 
   return process.exitCode = retCode;
